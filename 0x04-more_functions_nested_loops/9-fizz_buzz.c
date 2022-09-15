80efadcd644c6bb6f4f6a75acfab880891766961
Mayowa Ishola <mayowaolanipekun@yahoo.com>
2022-09-15 22:11:07 +0100

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,25 @@ int main(void)
 {
 	int k;
 
-	for (k = 1; k < 101; k++)
+	for (k = 1; k <= 100; k++)
 	{
-		if (k % 5 == 0 && k % 3 == 0)
-			printf("FizzBuzz ");
-		else if (k % 5 == 0)
-			if (k == 100)
+		if (k % 3 == 0 || k % 5 == 0)
+		{
+			if (k % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (k % 5 == 0)
+			{
 				printf("Buzz");
-			else
-				printf("Buzz ");
-		else if (k % 3 == 0)
-			printf("Fizz ");
-		else
-			printf("%d ", k);
+			}
+		} else
+		{
+			printf("%d", k);
+		}
+		if (k != 100)
+			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
