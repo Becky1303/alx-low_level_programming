ca288670d3c468cc1e3a19c4e4bca208c97e5057
Mayowa Ishola <mayowaolanipekun@yahoo.com>
2022-09-15 16:37:56 +0100

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* print_numbers - prints the numbers, from 0 to 9
-*				  followed by a new line
+* print_numbers - prints numbers from 0 to 9
+* followed by a new line
 *
 * Description: It prints numbers 0 to 9
 * 
 *
-* Return: 0
+* Return: void
 */
 void print_numbers(void)
 {
