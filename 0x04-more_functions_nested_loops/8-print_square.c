f8af5de7e616b59e65260635fc0e377383d8d1bb
Mayowa Ishola <mayowaolanipekun@yahoo.com>
2022-09-15 22:06:00 +0100

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,21 +9,21 @@
 */
 void print_square(int size)
 {
-	int a;
-	int b;
+	int i;
+	int j;
 
 	if (size <= 0)
-		_putchar('\n');
-	b = 0;
-	while (b < size)
 	{
-		a = 0;
-		while (a < size)
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
-			a++;
 		}
-		b++;
 	}
 }
