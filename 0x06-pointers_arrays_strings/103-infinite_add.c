f6e0bb45920c80e7560ec0d3cfa8ed3edcecab81
Mayowa Ishola <mayowaolanipekun@yahoo.com>
2022-09-21 20:48:16 +0100

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,50 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * _strlen - gets the length of a string
- * @s: the string variable
- *
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int z = 0;
-
-	while (*s++)
-		z++;
-	return (z);
-}
-
-/**
- * rev_string - reverses a string
- * @s: string to be reversed
- *
- * Return: void
- */
-char *rev_string(char *s)
-{
-	int l = _strlen(s), i = 0;
-	char u;
-
-	for (i = 0; i < l / 2; i++)
-	{
-		u = s[l - i - 1];
-		s[l - i - 1] = s[i];
-		s[i] = u;
-	}
-	return (s);
-}
-
-/**
- * infinite_add - adds long string of digits
- * @n1: first string
- * @n2: second string
- * @r: result buffer
- * @size_r: size of result buffer
- *
- * Return: char pointer to buffer
- */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
